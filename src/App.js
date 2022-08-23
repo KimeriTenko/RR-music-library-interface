d3718da3f0d4d34d373caf8cedfab4c47fd2cfd3
Mary Frye <deimospho@gmail.com>
2022-08-23 15:47:24 -0400

    ADDED a controlled form and fetch data from API, and render data to the screen, FIXED searchbar.js, datacontext.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import { useEffect, useState } from 'react'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+// import AlbumView from './components/AlbumView'
+// import ArtistView from './components/ArtistView'
+import { DataContext } from './context/DataContext'
 
-function App(){
+const App = () => {
     let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
 
+    const API_URL = 'https://itunes.apple.com/search?term='
+
         useEffect(() => {
+          if(search) {
             const fetchData = async () => {
                 document.title = `${search} Music`
-                const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+                const response = await fetch(API_URL + search)
                 const resData = await response.json()
-                if (ReadableStreamDefaultReader.results.length > 0) {
-                  setData ('Not Found')
+                if (resData.results.length > 0) {
+                  setData (resData.results)
+                } else {
+                  setMessage ('Not Found')
                 }
             }
             fetchData()
+          }
         }, [search])
-    
+
+        const handleSearch = (e, term) => {
+          e.preventDefault()
+          setSearch(term)
+      }
+      
         return (
-            <div>
-                <SearchBar />
-                {message}
-                <Gallery />
-            </div>
-        )
-    }
+          <div>
+              <SearchBar handleSearch = {handleSearch}/>
+              {message}
+              <DataContext.Provider value = {data}>
+                  <Gallery data = {data}/>
+              </DataContext.Provider>
+          </div>
+      );
+} 
     
-    export default App
+    export default App;
